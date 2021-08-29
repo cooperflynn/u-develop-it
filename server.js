9825324131a3453e256559fed402c7d753eea89c
Cooper <cooperflynn@utexas.edu>
2021-08-29 10:01:25 -0500

    add post route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -54,6 +55,7 @@ app.get('/api/candidate/:id', (req, res) => {
 app.delete('/api/candidate/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
     const params = [req.params.id];
+    //es5 function in order to be able to use 'this'
     db.run(sql, params, function(err, result) {
         if (err) {
             res.status(400).json({ error: err.message });
@@ -63,21 +65,35 @@ app.delete('/api/candidate/:id', (req, res) => {
         res.json({
             message: 'successfully deleted',
             changes: this.changes
-        })
-    })
-})
+        });
+    });
+});
 
-// Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//             VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// // ES5 function, not arrow function, so we can use 'this'
-// db.run(sql, params, function(err, result) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result, this.lastID);
-// })
+//Create a candidates
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                values (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        });
+    });
+});
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
